4670516f3632e7a45b38f4d15eee4418d73a233d
HUANG Wenchong <ebola@zju.edu.cn>
2023-09-28 23:27:48 +0800

    Fix

--- functions/DoublePendulum.h
+++ functions/DoublePendulum.h
@@ -12,12 +12,12 @@ public:
         res(1) = x(3);
         Matrix A(2,2);
         A(0,0) = 1+m2;
-        A(0,1) = m2*0.8*cos(x(0)-x(1));
-        A(1,0) = 0.8*cos(x(0)-x(1));
+        A(0,1) = m2*0.8*cosq(x(0)-x(1));
+        A(1,0) = 0.8*cosq(x(0)-x(1));
         A(1,1) = 0.8;
         ColVector rhs(2);
-        rhs(0) = (1+m2)*9.8*cos(x(0)) - m2*0.8*sin(x(0)-x(1))*x(3)*x(3);
-        rhs(1) = sin(x(0)-x(1))*x(2)*x(2) + 9.8*cos(x(1));
+        rhs(0) = (1+m2)*9.8*cosq(x(0)) - m2*0.8*sinq(x(0)-x(1))*x(3)*x(3);
+        rhs(1) = sinq(x(0)-x(1))*x(2)*x(2) + 9.8*cosq(x(1));
         ColVector sol = A.solve(rhs);
         res(2) = sol(0);
         res(3) = sol(1);
